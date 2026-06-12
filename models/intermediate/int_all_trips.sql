1611e6d78118f768f06646b97402825800e72b0f
manish39x <manishdolui35@gmail.com>
2026-06-12 11:42:09 +0000

    stage: intermidiate is done yaaaaa

---
+++ models/intermediate/int_all_trips.sql
@@ -0,0 +1,89 @@
+WITH yellow_taxi_data AS (
+    SELECT * FROM {{ ref('stg_taxi_trips__yellow') }}
+),
+green_taxi_data AS (
+    SELECT * FROM {{ ref('stg_taxi_trips__green') }}
+),
+all_trips AS (
+    SELECT * FROM yellow_taxi_data
+    UNION ALL
+    SELECT 
+        *,
+        NULL  AS airport_fee,
+        NULL  AS cbd_congestion_fee
+    FROM green_taxi_data
+),
+zone_lookup AS (
+    SELECT * FROM {{ ref('taxi_zone_lookup') }}
+),
+
+
+enriched AS (
+    SELECT
+    --  core trip columns from staging
+        t.taxi_type,
+        t.vendor_id,
+        t.pickup_datetime,
+        t.dropoff_datetime,
+        t.trip_duration_minutes,
+        t.passenger_count,
+        t.trip_distance,
+        t.store_and_fwd_flag,
+        t.payment_type,
+        t.fare_amount,
+        t.tip_amount,
+        t.total_amount,
+        t.rate_code_id,
+        t.extra,
+        t.mta_tax,
+        t.tolls_amount,
+        t.improvement_surcharge,
+        t.congestion_surcharge,
+        t.airport_fee,
+        t.cbd_congestion_fee,
+
+    --  decoded columns
+        CASE t.vendor_id
+            WHEN 1 THEN 'Creative Mobile Technologies, LLC'
+            WHEN 2 THEN 'Curb Mobility, LLC'
+            WHEN 6 THEN 'Myle Technologies Inc'
+            WHEN 7 THEN 'Helix'
+        END                                 AS vendor_name,
+        CASE t.rate_code_id
+            WHEN 1 THEN 'Standard rate'
+            WHEN 2 THEN 'JFK'
+            WHEN 3 THEN 'Newark'
+            WHEN 4 THEN 'Nassau or Westchester'
+            WHEN 5 THEN 'Negotiated fare'
+            WHEN 6 THEN 'Group ride'
+            WHEN 99 THEN  'Null/unknown'
+        END                                 AS rate_type,
+        CASE t.payment_type
+            WHEN 0 THEN 'Flex Fare trip'
+            WHEN 1 THEN 'Credit Card'
+            WHEN 2 THEN 'Cash'
+            WHEN 3 THEN 'No Charge'
+            WHEN 4 THEN 'Dispute'
+            WHEN 6 THEN 'Voided trip'
+            ELSE 'Unknown'
+        END                                 AS payment_type_disc,
+    --  time dimensions
+        DATE(t.pickup_datetime)            AS pickup_date,
+        YEAR(t.pickup_datetime)            AS pickup_year,
+        MONTH(t.pickup_datetime)            AS pickup_month,
+        HOUR(t.pickup_datetime)            AS pickup_HOUR,
+        CASE
+            WHEN DAYOFWEEK(t.pickup_datetime) IN (1, 7) THEN 'Weekend'
+            ELSE 'Weekday'
+        END                                AS day_type,
+    --  zone enrichment
+        p_up.zone                         AS pickup_zone,
+        p_up.borough                      AS pickup_borough,
+        d_off.zone                         AS dropoff_zone,
+        d_off.borough                      AS dropoff_borough
+    FROM all_trips t
+    LEFT JOIN zone_lookup p_up ON t.pickup_location_id = p_up.LocationID
+    LEFT JOIN zone_lookup d_off ON t.dropoff_location_id = d_off.LocationID
+)
+
+SELECT * FROM enriched
--- models/staging/stg_taxi_trips__green.sql
+++ models/staging/stg_taxi_trips__green.sql
@@ -1,25 +1,25 @@
 WITH source AS (
-    SELECT * FROM {{ source('src_nyc_taxi', 'yellow_taxi_trips') }}
+    SELECT * FROM {{ source('src_nyc_taxi', 'green_taxi_trips') }}
 ),
 
 renamed AS (
     SELECT
-    -- identifiers
-        'yellow'                                                                as taxi_type,
+    --  identifiers
+        'green'                                                                 as taxi_type,
         raw_data['VendorID']::INT                                               as vendor_id,
         raw_data['RatecodeID']::INT                                             as rate_code_id,
         raw_data['PULocationID']::INT                                           as pickup_location_id,
         raw_data['DOLocationID']::INT                                           as dropoff_location_id,
-    -- timestamps
-        TO_TIMESTAMP(raw_data['tpep_pickup_datetime']::NUMBER / 1000000, 0)        as pickup_datetime,
-        TO_TIMESTAMP(raw_data['tpep_dropoff_datetime']::NUMBER / 1000000, 0)       as dropoff_datetime,
-    -- trip info
+    --  timestamps
+        TO_TIMESTAMP(raw_data['lpep_pickup_datetime']::NUMBER / 1000000, 0)        as pickup_datetime,
+        TO_TIMESTAMP(raw_data['lpep_dropoff_datetime']::NUMBER / 1000000, 0)       as dropoff_datetime,
+    --  trip info
         raw_data['passenger_count']::INT                                           as passenger_count,
         raw_data['trip_distance']::FLOAT                                           as trip_distance,
         raw_data['store_and_fwd_flag']::VARCHAR                                    as store_and_fwd_flag,
         raw_data['payment_type']::INT                                              as payment_type,
-        DATEDIFF('minute', pickup_datetime, dropoff_datetime)                      as trip_duration_minute,
-    -- financials
+        DATEDIFF('minute', pickup_datetime, dropoff_datetime)                      as trip_duration,
+    --  financials
         raw_data['fare_amount']::FLOAT                                             as fare_amount,
         raw_data['extra']::FLOAT                                                   as extra,
         raw_data['mta_tax']::FLOAT                                                 as mta_tax,
@@ -28,13 +28,11 @@ renamed AS (
         raw_data['improvement_surcharge']::FLOAT                                   as improvement_surcharge,
         raw_data['total_amount']::FLOAT                                            as total_amount,
         raw_data['congestion_surcharge']::FLOAT                                    as congestion_surcharge,
-        raw_data['Airport_fee']::FLOAT                                          as airport_fee,
-        raw_data['cbd_congestion_fee']::FLOAT                                      as cbd_congestion_fee
     FROM source
     WHERE raw_data:trip_distance::FLOAT > 0
     AND raw_data:total_amount::FLOAT > 0
     AND raw_data:passenger_count::INT > 0
-    AND YEAR(TO_TIMESTAMP(raw_data:tpep_pickup_datetime::NUMBER / 1000000, 0)) BETWEEN 2014 AND 2026
+    AND DATE(TO_TIMESTAMP(raw_data:lpep_pickup_datetime::NUMBER / 1000000, 0)) BETWEEN DATE('2014-01-01') AND DATE('2026-03-01')
 )
 
 SELECT * FROM renamed
